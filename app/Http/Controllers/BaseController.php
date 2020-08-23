e1f1728c2e4ff5b5a995c82997a16e12100128aa
Teddy K <teddy@communic8agency.com>
2020-08-23 11:12:45 +0700

    add function import on base controller

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Validation\Rule;
 use Illuminate\Database\QueryException;
 use App\Exports\GeneralExport;
+use App\Imports\ImportExport;
 use Maatwebsite\Excel\Facades\Excel;
 
 class BaseController extends Controller
@@ -287,5 +288,28 @@ class BaseController extends Controller
         $model = new $this->model;
         
         return Excel::download(new GeneralExport($this->model, $column, $requestQuery['from'] ?? null, $requestQuery['to'] ?? null), ucfirst(str_replace("_", " ", $model->getTable())).'.xlsx');
+    }
+
+    public function import() 
+    {
+        $validator = Validator::make(request()->all(),[
+            'file_excel' => 'required|mimes:xlsx,xls',
+        ]);
+
+        try{
+            if ($validator->fails()) {
+                throw new ValidationException($validator);
+            }
+
+            Excel::import(new GeneralImport, request()->file('file'));
+
+            return response()->json([
+                'message' => 'Import file excel success.'
+            ], 200);
+        } catch(Exception $e){
+            return response()->json([
+                'message' => $e->getMessage()
+            ], 400);
+        }
     }
 }
